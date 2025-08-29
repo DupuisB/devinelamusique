780c6a3da272cf0bfc3ea4a063f18317bfd39da8
Benjamin <68610598+DupuisB@users.noreply.github.com>
2025-08-29 20:50:44 +0200

    test

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 // Central app configuration for the daily game
 // You can change these values without touching the code.
 
-export const START_DATE_UTC = '2025-08-22' // YYYY-MM-DD (UTC) — day #1
+export const START_DATE_UTC = '2025-05-22' // YYYY-MM-DD (UTC) — day #1
 
 export const FR_PLAYLIST_URL = 'https://www.deezer.com/fr/playlist/14230090421'
 export const EN_PLAYLIST_URL = 'https://www.deezer.com/fr/playlist/7873409502'
